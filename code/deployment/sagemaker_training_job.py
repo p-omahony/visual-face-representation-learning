f488e34743bb6e43648b720237cff81237844eae
Paul O’Mahony <paul@atyos.io>
2024-08-26 10:45:50 +0200

    add train action

--- code/deployment/sagemaker_training_job.py
+++ code/deployment/sagemaker_training_job.py
@@ -5,14 +5,14 @@ import os
 import boto3
 import boto3.session
 import sagemaker
-from dotenv import load_dotenv
 from sagemaker.estimator import Estimator
 
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 AWS_PROFILE_NAME = os.getenv('AWS_PROFILE_NAME')
 
-IMAGE =  os.getenv('SAGEMAKER_TRAINING_JOB_IMAGE')
+IMAGE =  os.getenv('IMAGE_ECR_URI')
 ROLE = os.getenv('SAGEMAKER_EXECUTION_ROLE')
 DATA_LOCATION = os.getenv('SAGEMAKER_TJ_INPUT_DATA_PATH')
 OUTPUT_PATH = os.getenv('SAGEMAKER_TJ_OUTPUT_PATH')
